81427744485c9f6da4e5837a4b2567bd17240950
StrayKite\jonathan <jonathan@straykitestudios.com>
2022-12-07 14:39:25 -0600

    Cant use a dictionary jonathan. Unique file names Jonathan.

--- Source/Day7.py
+++ Source/Day7.py
@@ -1,10 +1,5 @@
 from AoCUtilities import *
 from collections import Counter
-from enum import Enum
-
-class LineMode(Enum):
-    ChangeDir = 1
-    ListDir = 2
 
 class Directory:
     def __init__(self, newDirName, newPrevDir):
@@ -19,8 +14,17 @@ class Directory:
         newDir.files = self.files.copy()
         return newDir
 
-    def GetDirs(self):
+    def GetName(self) -> str:
+        return self.dirName
+
+    def GetDirs(self) -> list['Directory']:
         return self.dirs
+
+    def GetDir(self, dirName) -> 'Directory':
+        for dir in self.dirs:
+            if dir.dirName == dirName:
+                return dir
+        return None
 
     def GetPrevDir(self):
         return self.prevDir
@@ -34,90 +38,150 @@ class Directory:
     def AddFile(self, fileName, fileSize):
         self.files[fileName] = fileSize
 
+    def ContainsDir(self, dirName: str):
+        for dir in self.dirs:
+            if dir.dirName == dirName:
+                return True
+        return False
+
     def GetDirSize(self):
         dirSize = 0
         for key, value in self.files.items():
             dirSize += int(value)
         return dirSize
 
-    def GetDirSizeRecursive(self, dirDict : dict):
+    def GetDirSizeRecursive(self):
         currentDirSize = self.GetDirSize()
         for dir in self.dirs:
-            currentDirSize += dirDict[dir].GetDirSizeRecursive(dirDict)
+            currentDirSize += dir.GetDirSizeRecursive()
         return currentDirSize
 
     dirName = ''
-    prevDir = ''
+    prevDir = None
     dirs = []
     files = {}
+
 
 def GetLastColStr(cols):
     lastColStr = ''
 
     for col in cols:
-        lastColStr += col[len(col)-1]
+        lastColStr += col[len(col) - 1]
 
     return lastColStr
+
+
+def TraverseDirectory(rootDir: 'Directory'):
+    totalSum = 0
+    currentSize = rootDir.GetDirSizeRecursive()
+    if currentSize < 100000:
+        totalSum += currentSize
+
+    for dir in rootDir.dirs:
+        totalSum += TraverseDirectory(dir)
+    return totalSum
+
+
+def GetLowestFileSizeOverMin(rootDir: 'Directory', minFileSize):
+    currentBestFileSize = -1
+
+    dirSize = rootDir.GetDirSizeRecursive()
+
+    if dirSize > minFileSize:
+        currentBestFileSize = dirSize
+
+    for dir in rootDir.dirs:
+        bestFileSize = GetLowestFileSizeOverMin(dir, minFileSize)
+        if bestFileSize > minFileSize and bestFileSize < currentBestFileSize:
+            currentBestFileSize = bestFileSize
+
+    return currentBestFileSize
+
 
 def SolveDayPartA(filepath):
     with open(filepath, "r") as openedFile:
         fileData = openedFile.readlines()
 
-    dirDict = {}
-
+    rootDir = None
     currentDirName = ''
-    currentDir = Directory('','')
+    currentDir = Directory('', None)
 
-    #LineMode currentMode = LineMode()
+    # LineMode currentMode = LineMode()
 
     for fileLine in fileData:
         lineData = fileLine.strip().split(' ')
         if lineData[0] == '$':
-            if lineData[1] == 'cd': #Still need to handle cd ..
+            if lineData[1] == 'cd':
                 if currentDirName == '':
                     currentDirName = lineData[2]
-                    if currentDirName in dirDict.keys():
-                        currentDir = dirDict[currentDirName].copy()
-                    else:
-                        currentDir = Directory(currentDirName, '')
+                    currentDir = Directory(currentDirName, None)
+                    rootDir = currentDir
+
                 elif lineData[2] == '..':
-                    newDirName = currentDir.GetPrevDir()
-                    if newDirName == '':
-                        continue
-                    currentDir = dirDict[newDirName].copy()
-                    currentDirName = newDirName
+                    currentDir = currentDir.GetPrevDir()
+                    currentDirName = currentDir.GetName()
                 else:
-                    prevDir = currentDirName
-                    currentDirName = lineData[2]
-                    if currentDirName in dirDict.keys():
-                        currentDir = dirDict[currentDirName].copy()
-                    else:
-                        newDir = Directory(currentDirName, prevDir)
-                        currentDir = newDir.copy()
-                dirDict[currentDirName] = currentDir.copy()
+                    currentDir = currentDir.GetDir(lineData[2])
+                    currentDirName = currentDir.GetName()
 
             elif lineData[1] == 'ls':
-                #currentMode = LineMode.ListDir
+                # currentMode = LineMode.ListDir
                 continue
         elif lineData[0] == 'dir':
-            currentDir.AddDir(lineData[1])
-            dirDict[currentDirName] = currentDir.copy()
+            newDir = Directory(lineData[1], currentDir)
+            currentDir.AddDir(newDir)
         else:
             currentDir.AddFile(lineData[1], lineData[0])
-            dirDict[currentDirName] = currentDir.copy()
 
-    sumOfSizes = 0
-    for key, value in dirDict.items():
-        currentSize = value.GetDirSizeRecursive(dirDict)
-        if currentSize < 100000:
-            sumOfSizes += currentSize
+    sumOfSizes = TraverseDirectory(rootDir)
     return sumOfSizes
+
 
 def SolveDayPartB(filepath):
     with open(filepath, "r") as openedFile:
         fileData = openedFile.readlines()
 
-    return 0
+    rootDir = None
+    currentDirName = ''
+    currentDir = Directory('', None)
+
+    # LineMode currentMode = LineMode()
+
+    for fileLine in fileData:
+        lineData = fileLine.strip().split(' ')
+        if lineData[0] == '$':
+            if lineData[1] == 'cd':
+                if currentDirName == '':
+                    currentDirName = lineData[2]
+                    currentDir = Directory(currentDirName, None)
+                    rootDir = currentDir
+
+                elif lineData[2] == '..':
+                    currentDir = currentDir.GetPrevDir()
+                    currentDirName = currentDir.GetName()
+                else:
+                    currentDir = currentDir.GetDir(lineData[2])
+                    currentDirName = currentDir.GetName()
+
+            elif lineData[1] == 'ls':
+                # currentMode = LineMode.ListDir
+                continue
+        elif lineData[0] == 'dir':
+            newDir = Directory(lineData[1], currentDir)
+            currentDir.AddDir(newDir)
+        else:
+            currentDir.AddFile(lineData[1], lineData[0])
+
+    totalSpace = 70000000
+    neededSpace = 30000000
+
+    usedSpace = rootDir.GetDirSizeRecursive()
+    currentUnusedSpace = totalSpace - usedSpace
+    spaceToFreeUp = neededSpace - currentUnusedSpace
+
+    lowestSizeOverMin = GetLowestFileSizeOverMin(rootDir, spaceToFreeUp)
+    return lowestSizeOverMin
+
 
 filePath = "C:\\dev\\AdventOfCode2022\\Input\\Day7.txt"
 print_count(SolveDayPartA(filePath))
